d6c85a09c6e2e3d4be5193cf7cdb0887bd0c7bc1
Yuriy Zabegaev <zabegaev99@gmail.com>
2018-08-20 11:45:18 +0300

    fixed newgame bug

---
+++ SetGame/Card.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  SetGame
+//
+//  Created by Юрий Забегаев on 20.08.2018.
+//  Copyright © 2018 Юрий Забегаев. All rights reserved.
+//
+
+import UIKit
+
+class Card: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
